f114195dadeafae944f802113fbd40031772dd34
mrfearless <fearless.is.my.name@gmail.com>
2023-10-21 15:56:22 +0100

    Revert "Revert "initial upload""
    This reverts commit d0a9e2740c44e9e9d0747e50324893294466929c.

---
+++ Images/Exit_Button.bmp.asm
@@ -0,0 +1,36 @@
+;------------------------------------------------------------------------------
+; CD - Compress-Decompress Utility by fearless
+;
+; Source Filename  : Exit_Button.bmp
+; Size Uncompressed: 6198 Bytes
+; Size Compressed  : 332 Bytes
+; Compression Ratio: 95% (19:1)
+; Algorithm Used   : LZMS
+;------------------------------------------------------------------------------
+
+.DATA
+
+EXIT_BUTTON \
+DB 04Ch, 05Ah, 04Dh, 053h, 00Ah, 051h, 0E5h, 0C0h, 018h, 000h, 0C7h, 005h, 036h, 018h, 000h, 000h
+DB 000h, 000h, 000h, 000h, 036h, 018h, 000h, 000h, 000h, 000h, 000h, 000h, 02Ch, 001h, 000h, 000h
+DB 076h, 037h, 0C8h, 019h, 0DDh, 037h, 06Fh, 08Ah, 0E7h, 077h, 001h, 0AEh, 0F7h, 0B6h, 098h, 012h
+DB 0AEh, 04Dh, 043h, 0FCh, 031h, 0B8h, 022h, 05Ah, 02Eh, 04Fh, 002h, 092h, 084h, 0A4h, 084h, 00Fh
+DB 001h, 060h, 048h, 099h, 05Bh, 01Bh, 0A0h, 0F7h, 082h, 061h, 0A4h, 081h, 052h, 0D8h, 029h, 0A1h
+DB 0F9h, 07Dh, 0B9h, 07Ah, 06Ah, 018h, 098h, 067h, 062h, 02Eh, 038h, 0E5h, 0C6h, 080h, 045h, 0ECh
+DB 0D3h, 051h, 01Bh, 00Fh, 000h, 000h, 000h, 030h, 017h, 024h, 040h, 073h, 075h, 0F1h, 020h, 0F7h
+DB 0F2h, 0D9h, 0DAh, 0B6h, 0E6h, 0ABh, 043h, 077h, 04Bh, 058h, 036h, 096h, 09Dh, 025h, 05Dh, 07Dh
+DB 0FBh, 043h, 07Bh, 0B8h, 07Ah, 05Dh, 00Ch, 0EDh, 0BDh, 021h, 05Ah, 0D8h, 020h, 078h, 068h, 07Fh
+DB 04Ch, 0D3h, 0F2h, 046h, 061h, 0CBh, 0ABh, 0D6h, 078h, 0FFh, 01Bh, 0A7h, 07Eh, 070h, 098h, 0B9h
+DB 04Dh, 0E4h, 0DFh, 00Ah, 0AEh, 0B3h, 06Dh, 0D6h, 0FAh, 038h, 0DEh, 004h, 0AEh, 0A5h, 004h, 056h
+DB 09Fh, 075h, 02Fh, 05Eh, 010h, 0D6h, 0AAh, 0BDh, 008h, 061h, 045h, 07Eh, 0CCh, 020h, 0E8h, 073h
+DB 058h, 09Dh, 030h, 000h, 05Eh, 030h, 02Ch, 059h, 0CBh, 0DCh, 0CBh, 0FFh, 0EEh, 0B7h, 02Ch, 07Ah
+DB 018h, 0C8h, 0FAh, 0FDh, 098h, 001h, 0BDh, 069h, 0A3h, 0FFh, 003h, 097h, 096h, 0AAh, 0D3h, 058h
+DB 0B3h, 0DCh, 004h, 028h, 0D6h, 0A9h, 0B8h, 0EFh, 0F5h, 00Ah, 002h, 087h, 0B5h, 049h, 003h, 084h
+DB 0D5h, 025h, 0CDh, 0EBh, 00Ah, 000h, 003h, 001h, 0C8h, 0FEh, 00Fh, 07Dh, 0F2h, 0A3h, 017h, 01Eh
+DB 0B7h, 056h, 001h, 0ACh, 0DDh, 056h, 0A6h, 0A5h, 065h, 079h, 058h, 0FDh, 08Fh, 0D7h, 0D6h, 0A1h
+DB 085h, 064h, 061h, 058h, 005h, 0B6h, 0BCh, 04Dh, 039h, 0DAh, 0AAh, 0B3h, 06Ch, 0B4h, 0D3h, 026h
+DB 000h, 070h, 0FDh, 05Bh, 0CBh, 0D6h, 098h, 045h, 062h, 031h, 058h, 047h, 066h, 000h, 050h, 06Ch
+DB 045h, 059h, 012h, 096h, 081h, 020h, 013h, 093h, 0F1h, 000h, 018h, 016h, 0D6h, 00Ch, 0B0h, 040h
+DB 02Ch, 018h, 00Bh, 0C5h, 05Ah, 0DAh, 060h, 000h, 018h, 036h, 04Dh, 042h
+
+EXIT_BUTTONLength DD $ - EXIT_BUTTON
